fd610a66ec8c1911c2005fdb32750dbd30461c10
GermanBobikTheThird <germanbobik3@gmail.com>
2025-10-31 17:17:32 +0300

    refactor: move constants to constants.js

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,1 +1,14 @@
-
+export const MOBILE_DEVICE_REGEX = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+export const BREAKPOINT = 768;
+export const MAX_STARS_MOBILE = 50;
+export const MAX_STARS_DESKTOP = 150;
+export const DENSITY_DIVIDER_MOBILE = 5000;
+export const DENSITY_DIVIDER_DESKTOP = 3000;
+export const RESIZE_DEBOUNCE_DELAY = 250;
+export const UPDATE_DEBOUNCE_DELAY = 100;
+export const ANIMATION_DURATION = { MIN: 2, MAX: 6 };
+export const ANIMATION_DELAY_MAX = 5;
+export const NUMBER_INPUT = { MIN: 1, MAX: 666 };
+export const DEFAULT_RESULT_MESSAGE = "Здесь появится результат кодирования...";
+export const EMPTY_RESULT_MESSAGE = "Нет данных для отображения";
+export const DEFAULT_NUMBER_OF_OPTIMAL_VALUES = 5;
